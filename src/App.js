2474bcdc908f304c37865db7a528210dcdbc32a3
TkreateDev <tanbir98@hotmail.it>
2021-10-07 12:57:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components';
+import convert from 'xml-js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+  const [data, setData] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState(null)
+
+  React.useEffect(() =>
+    fetch('https://front-end-test-bvhzjr6b6a-uc.a.run.app/')
+      .then(res => res.json())
+      .then(val => setData(val))
+      .catch(err => setError(err))
+      .then(() => setIsLoading(false))
+    , [])
+
+  return(
+    isLoading ? <Loading /> :
+      <AppContainer>
+        {data.map(({...rest}, key) => 
+          <ProductItem {...rest} key={key}/>
+      )}
+      </AppContainer>
+  )
 }
+
+const Loading = () => {
+  return(
+    <div>loading</div>
+  )
+}
+const IsJsonString = string => {
+    try {
+        JSON.parse(string);
+    } catch (e) {
+        return false;
+    }
+    return true;
+  }
+
+const ProductItem = ({currency, price, productImage}) => {
+  return(
+    <ItemContainer>
+      <Image src={productImage}/>
+      <Price>{price} {currency}</Price>
+      <CategoryContainer>
+
+      </CategoryContainer>
+    </ItemContainer>
+  )
+}
+
+const Price = styled.span`
+  font-size: 30px;
+  font-weight: 600;
+  font-family: sans-serif;
+`
+
+const CenteredBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const AppContainer = styled(CenteredBox)`
+  min-height: 100vh;
+  width: 100vw;
+  flex-flow: row wrap;
+`
+
+const CategoryContainer = styled(CenteredBox)`
+  flex-flow: row wrap;
+`
+
+const ItemContainer = styled(CenteredBox)`
+  width: 100%;
+  height: 400px;
+  padding: 20px;
+  background-color: lightgray;
+  flex-flow: column nowrap;
+  @media only screen and (min-width: 400px) {
+    width: 400px;
+    margin: 20px;
+  }
+`
+const Image = styled.img`
+  width: 300px;
+  margin: 10px;
+`
 
 export default App;
